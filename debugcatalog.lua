b366c57ac9d8a1ba8f1daac5e404ab25a319daeb
Carlo Acutis FTW <117101610+HOAsAreFullOfComunists@users.noreply.github.com>
2024-12-13 08:05:41 +0100

    Der katalog

---
+++ debugcatalog.lua
@@ -0,0 +1,233 @@
+--------------------------------------------------------------------------------
+-- Debug catalog data
+--------------------------------------------------------------------------------
+
+if useDebugCatalog then
+	debugCatalog = [[{
+	"head": {
+		"protocolVersion": "1.0",
+		"apiVersion": "1.0",
+		"playerDevMode": false,
+		"fromCache": true,
+		"serverName": "",
+		"processTime": "3.46ms",
+		"memoryUsage": "1.07 MB",
+		"memoryPeakUsage": "1.12 MB"
+	},
+	"body": [
+		{
+			"setNode": {
+				"id": "root",
+				"type": "root",
+				"children": [
+					{
+						"id": "channel:category1",
+						"version": "7a6f5387cea37c915a15b50a16214cd7"
+					}
+				],
+				"version": "73dc61d9b2b6bfb373e4c0ae8bb9a5ae"
+			}
+		},
+		{
+		   "setNode": {
+		      "id": "episode:5Gum1",
+		      "version": "649f671be9bd0e2bf9fa18d73de07d57",
+		      "type": "episode",
+		      "number": 1,
+		      "ageRate": 0,
+		      "title": "5 Gum",
+		      "description": "",
+		      "mediaUrls": {
+		         "default": "https://archive.org/download/bitte-esst-keine-besen/5%20Gum.moflex"
+		      },
+		      "viewCount": 111,
+		      "imageUrls": {
+		         "default": "sdmc:/luma/thumbs/5Gum.3dst"
+		      },
+		      "agePrerollVideo": ""
+		   }
+		},
+		{
+		   "setNode": {
+		      "id": "episode:Besen1",
+		      "version": "649f671be9bd0e2bf9fa18d73de07d57",
+		      "type": "episode",
+		      "number": 2,
+		      "ageRate": 0,
+		      "title": "Bitte, esst keine Besen.",
+		      "description": "",
+		      "mediaUrls": {
+		         "default": "https://archive.org/download/bitte-esst-keine-besen/Bitte%20esst%20keine%20Besen.moflex"
+		      },
+		      "viewCount": 111,
+		      "imageUrls": {
+		         "default": "sdmc:/luma/thumbs/Besen.3dst"
+		      },
+		      "agePrerollVideo": ""
+		   }
+		},
+		{
+		   "setNode": {
+		      "id": "episode:GustavGustavsn1",
+		      "version": "649f671be9bd0e2bf9fa18d73de07d57",
+		      "type": "episode",
+		      "number": 3,
+		      "ageRate": 0,
+		      "title": "Hackerman!",
+		      "description": "",
+		      "mediaUrls": {
+		         "default": "https://archive.org/download/bitte-esst-keine-besen/Hackerman.moflex"
+		      },
+		      "viewCount": 111,
+		      "imageUrls": {
+		         "default": "sdmc:/luma/thumbs/hackerman.3dst"
+		      },
+		      "agePrerollVideo": ""
+		   }
+		},
+		{
+		   "setNode": {
+		      "id": "episode:KlautFahrraeder1",
+		      "version": "649f671be9bd0e2bf9fa18d73de07d57",
+		      "type": "episode",
+		      "number": 4,
+		      "ageRate": 0,
+		      "title": "Klaut keine Fahrräder",
+		      "description": "",
+		      "mediaUrls": {
+		         "default": "https://archive.org/download/bitte-esst-keine-besen/Klaut%20Keine%20Fahrr%C3%A4der.moflex"
+		      },
+		      "viewCount": 111,
+		      "imageUrls": {
+		         "default": "sdmc:/luma/thumbs/Rad.3dst"
+		      },
+		      "agePrerollVideo": ""
+		   }
+		},
+		{
+		   "setNode": {
+		      "id": "episode:ElonMuskIstLostAF1",
+		      "version": "649f671be9bd0e2bf9fa18d73de07d57",
+		      "type": "episode",
+		      "number": 5,
+		      "ageRate": 0,
+		      "title": "Ein Langes Rohr",
+		      "description": "",
+		      "mediaUrls": {
+		         "default": "https://archive.org/download/bitte-esst-keine-besen/Ein%20Langes%20Rohr.moflex"
+		      },
+		      "viewCount": 111,
+		      "imageUrls": {
+		         "default": "sdmc:/luma/thumbs/Rohr.3dst"
+		      },
+		      "agePrerollVideo": ""
+		   }
+		},
+		{
+		   "setNode": {
+		      "id": "episode:PSP4Eva1",
+		      "version": "649f671be9bd0e2bf9fa18d73de07d57",
+		      "type": "episode",
+		      "number": 6,
+		      "ageRate": 0,
+		      "title": "Die Beste Konsole Aller Zeiten",
+		      "description": "",
+		      "mediaUrls": {
+		         "default": "https://archive.org/download/bitte-esst-keine-besen/Die%20Beste%20Konsole%20Aller%20Zeiten.moflex"
+		      },
+		      "viewCount": 111,
+		      "imageUrls": {
+		         "default": "sdmc:/luma/thumbs/PPSSPP.3dst"
+		      },
+		      "agePrerollVideo": ""
+		   }
+		},
+		{
+		   "setNode": {
+		      "id": "episode:Verweis1",
+		      "version": "649f671be9bd0e2bf9fa18d73de07d57",
+		      "type": "episode",
+		      "number": 7,
+		      "ageRate": 0,
+		      "title": "Mein Schulverweis",
+		      "description": "",
+		      "mediaUrls": {
+		         "default": "https://archive.org/download/bitte-esst-keine-besen/Mein%20Schulverweis.moflex"
+		      },
+		      "viewCount": 111,
+		      "imageUrls": {
+		         "default": "sdmc:/luma/thumbs/Verweis.3dst"
+		      },
+		      "agePrerollVideo": ""
+		   }
+		},
+		{
+		   "setNode": {
+		      "id": "episode:Sozialstunden0",
+		      "version": "649f671be9bd0e2bf9fa18d73de07d57",
+		      "type": "episode",
+		      "number": 8,
+		      "ageRate": 0,
+		      "title": "Sozialstunden",
+		      "description": "",
+		      "mediaUrls": {
+		         "default": "https://archive.org/download/bitte-esst-keine-besen/Sozialstunden.moflex"
+		      },
+		      "viewCount": 111,
+		      "imageUrls": {
+		         "default": "sdmc:/luma/thumbs/Stunden.3dst"
+		      },
+		      "agePrerollVideo": ""
+		   }
+		},
+
+
+		{
+			"setNode": {
+				"id": "channel:category1",
+				"type": "channel",
+				"title": "Plankon > anime",
+				"eshopLinks": "",
+				"imageUrls": {
+					"default": "https://dm13bvvnwveun.cloudfront.net/inazuma/thumbs/IE03-thumbnail.3dst"
+				},
+				"children": [
+					{
+					 "id": "episode:5Gum1",
+					 "version": "55d29a3bf5f0ed8b98234f2b88911f4c"
+					},
+					{
+					 "id": "episode:Besen1",
+					 "version": "55d29a3bf5f0ed8b98234f2b88911f4c"
+					},
+					{
+					 "id": "episode:GustavGustavsn1",
+					 "version": "55d29a3bf5f0ed8b98234f2b88911f4c"
+					},
+					{
+					 "id": "episode:KlautFahrraeder1",
+					 "version": "55d29a3bf5f0ed8b98234f2b88911f4c"
+					},
+					{
+					 "id": "episode:ElonMuskIstLostAF1",
+					 "version": "55d29a3bf5f0ed8b98234f2b88911f4c"
+					},
+					{
+					 "id": "episode:PSP4Eva1",
+					 "version": "55d29a3bf5f0ed8b98234f2b88911f4c"
+					},
+					{
+					 "id": "episode:Verweis1",
+					 "version": "55d29a3bf5f0ed8b98234f2b88911f4c"
+					},
+					{
+					 "id": "episode:Sozialstunden0",
+					 "version": "55d29a3bf5f0ed8b98234f2b88911f4c"
+					}
+				],
+				"version": "7a6f5387cea37c915a15b50a16214cd7"
+			}
+		}
+	]
+}]]
+end
